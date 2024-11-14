f1ace55e45a1d3a348f04efe7d7fe56b0a4e66fa
NarciSource <jwch11@gmail.com>
2024-11-14 20:53:46 +0900

    feat: login 서비스 구현
    - 사용 예시도 추가

---
+++ src/features/auth/services/login/login.usage.ts
@@ -0,0 +1,14 @@
+import { login } from "./login";
+
+(async function () {
+    try {
+        const resultMessage = await login({
+            username: "testuser",
+            password: "Abcde12345**",
+        });
+
+        console.log(resultMessage);
+    } catch (e) {
+        console.error(e);
+    }
+})();
