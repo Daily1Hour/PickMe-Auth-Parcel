aaa126b9326e0e812fdf559c3078264f2edc8723
NarciSource <jwch11@gmail.com>
2024-11-15 16:11:03 +0900

    chore: react-query 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider as ChakraProvider } from "@/shared/chakra-ui/provider";
 
 import AuthPage from "@/pages/auth";
 
+const queryClient = new QueryClient();
+
 function App() {
     return (
-        <ChakraProvider>
-            <AuthPage />
-        </ChakraProvider>
+        <QueryClientProvider client={queryClient}>
+            <ChakraProvider>
+                <AuthPage />
+            </ChakraProvider>
+        </QueryClientProvider>
     );
 }
 
