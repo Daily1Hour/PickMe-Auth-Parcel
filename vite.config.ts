7542bc644661d469e393f80092ea509d4da51614
NarciSource <jwch11@gmail.com>
2024-11-15 11:05:12 +0900

    chore: Vite Polyfill 설정
    - nodejs 내장모듈을 브라우저에서도 동작하도록 변환해주기 위해서
    - vite 기본 path를 tsconfig에서 정의한 path로 연결하기 위해서

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,14 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
     return {
-        plugins: [react()],
+        plugins: [react(), nodePolyfills(), tsconfigPaths()],
         base: process.env.VITE_PUBLIC_URL,
-        resolve: {
-            alias: {
-                "@": path.resolve(__dirname, "src"),
-            },
-        },
     };
 });
