cb5c75ec9b5c3236500c95ca2ca4b2017d91da45
NarciSource <jwch11@gmail.com>
2024-11-15 18:14:14 +0900

    feat: 현재 로그인 확인

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Flex, Box } from "@chakra-ui/react";
+import { Flex, Box, Button } from "@chakra-ui/react";
 
 import LoginPresentation from "./ui/LoginPresentation";
 import SignupPresentation from "./ui/SignupPresentation";
@@ -8,21 +8,29 @@ import useLoginForm from "./ui/hooks/useLoginForm";
 import useSignupForm from "./ui/hooks/useSignupForm";
 import useLoginFetch from "./api/useLoginFetch";
 import useSignupFetch from "./api/useSignupFetch";
+import getLoggedIn from "@/shared/lib/getLoggedIn";
 
 export default function AuthPage(): React.ReactElement {
     const loginFormMethods = useLoginForm();
     const signupFormMethods = useSignupForm();
     const { handleSubmit: handleLoginSubmit } = useLoginFetch();
     const { handleSubmit: handleSignupSubmit } = useSignupFetch();
+    const { isLoggedIn, logoutHandler } = getLoggedIn();
 
     return (
         <Flex bg="gray.100" p={3}>
-            <Box flex="5">
-                <LoginPresentation {...loginFormMethods} onSubmit={handleLoginSubmit} />
-            </Box>
-            <Box flex="5">
-                <SignupPresentation {...signupFormMethods} onSubmit={handleSignupSubmit} />
-            </Box>
+            {!isLoggedIn ? (
+                <>
+                    <Box flex="5">
+                        <LoginPresentation {...loginFormMethods} onSubmit={handleLoginSubmit} />
+                    </Box>
+                    <Box flex="5">
+                        <SignupPresentation {...signupFormMethods} onSubmit={handleSignupSubmit} />
+                    </Box>
+                </>
+            ) : (
+                <Button onClick={logoutHandler}>로그아웃</Button>
+            )}
         </Flex>
     );
 }
---
+++ src/shared/lib/getLoggedIn.ts
@@ -0,0 +1,21 @@
+import userPoolData from "@/features/auth/services/userPoolData";
+import { CognitoUserPool } from "amazon-cognito-identity-js";
+
+export default function getLoggedIn(): { isLoggedIn: boolean; logoutHandler: () => void } {
+    // CognitoUserPool 인스턴스를 생성
+    const userPool = new CognitoUserPool(userPoolData);
+    // 현재 로그인한 사용자를 가져옴
+    // localStorage에 저장된 토큰을 사용하여 사용자 정보를 가져옴
+    const cognitoUser = userPool.getCurrentUser();
+    // 사용자가 로그인했는지 정의
+    const isLoggedIn = !!cognitoUser;
+
+    const logoutHandler = () => {
+        if (cognitoUser) {
+            cognitoUser.signOut();
+            window.location.reload();
+        }
+    };
+
+    return { isLoggedIn, logoutHandler };
+}
