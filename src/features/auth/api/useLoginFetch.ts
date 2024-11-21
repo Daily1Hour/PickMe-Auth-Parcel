1674d4fd4c22dbc06eec9e4b27e5f3b13303c736
NarciSource <jwch11@gmail.com>
2024-11-21 18:05:41 +0900

    fix: 로그인 성공 시 isLoggedIn 쿼리 재요청 추가
    - 로그인 성공 후에 재랜더링 안되는 문제제

--- src/features/auth/api/useLoginFetch.ts
+++ src/features/auth/api/useLoginFetch.ts
@@ -1,13 +1,20 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import LoginCredential from "@/entities/auth/model/LoginCredential";
 import login from "@/entities/auth/service/login/login";
 
 export default function useLoginFetch() {
+    const queryClient = useQueryClient();
+
     const { mutate } = useMutation({
         mutationFn: login,
-        onSuccess: () => {
+        onSuccess: async () => {
             console.log("Login success");
+
+            // 로그인 성공 시 isLoggedIn 쿼리를 다시 불러옴
+            await queryClient.refetchQueries({
+                queryKey: ["isLoggedIn"],
+            });
         },
     });
 
