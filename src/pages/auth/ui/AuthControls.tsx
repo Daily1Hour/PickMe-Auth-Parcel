26fd28480bd0a468fc080030e563e41ae11e6f29
NarciSource <jwch11@gmail.com>
2024-11-30 21:22:32 +0900

    refactor: 환경 변수 수정

--- src/pages/auth/ui/AuthControls.tsx
+++ src/pages/auth/ui/AuthControls.tsx
@@ -3,14 +3,14 @@ import React from "react";
 import { Flex } from "@chakra-ui/react";
 
 import AuthActions from "@/features/authActions";
-import UserInfo, { useLoggedIn } from "@/features/userMenu";
+import UserMenu, { useLoggedIn } from "@/features/userMenu";
 
 export default function AuthControls(): React.ReactElement {
     const { isLoggedIn } = useLoggedIn();
 
     return (
         <Flex w="100%" p={3} justifyContent="right">
-            {!isLoggedIn ? <AuthActions /> : <UserInfo />}
+            {!isLoggedIn ? <AuthActions /> : <UserMenu />}
         </Flex>
     );
 }
