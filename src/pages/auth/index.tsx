463eeac78e91d09506a0e777e924ca2acb93727f
NarciSource <jwch11@gmail.com>
2025-02-04 21:38:22 +0900

    feat: Chakra UI 토스터 컴포넌트 추가

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Box } from "@chakra-ui/react";
+import { Toaster } from "@/third-party/chakra-ui";
 
 import { useLoggedIn } from "@/features/userMenu";
 import { AuthControls, TokenInfo } from "./ui";
@@ -16,6 +17,8 @@ export default function AuthPage(): React.ReactElement {
             </Box>
 
             {isTokenView && isLoggedIn && <TokenInfo />}
+
+            <Toaster />
         </>
     );
 }
