38357e706de1fe24ea0ec83b123667f8b1eb8621
NarciSource <jwch11@gmail.com>
2025-02-08 15:28:47 +0900

    fix: 빌드 타겟 명시

--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,7 @@ export default defineConfig(({ mode }) => {
         ],
         build: {
             emptyOutDir: false,
+            target: "esnext",
         },
         server: {
             // 개발 서버 설정
