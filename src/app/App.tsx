5bd9e9dc3612b574cf5425a30940589387ea2152
NarciSource <jwch11@gmail.com>
2025-02-12 16:34:17 +0900

    fix: Chakra UI 시스템의 비동기 로딩 처리

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,23 @@
+import { useEffect, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ChakraProvider } from "@chakra-ui/react";
-import chakraUiSystem from "@styleguide/chakra-ui-system";
+import { ChakraProvider, SystemContext } from "@chakra-ui/react";
 
 import AuthPage from "@/pages/auth";
 
 const queryClient = new QueryClient();
 
 function App() {
+    const [chakraUiSystem, setChakraUiSystem] = useState<SystemContext | null>(null);
+
+    // Chakra UI 시스템의 비동기 로딩 처리
+    useEffect(() => {
+        import("@styleguide/react").then((mod) => {
+            setChakraUiSystem(mod.chakraUiSystem);
+        });
+    }, []);
+
+    if (!chakraUiSystem) return <div>Loading UI...</div>;
+
     return (
         <QueryClientProvider client={queryClient}>
             <ChakraProvider value={chakraUiSystem}>
--- src/parcel.tsx
+++ src/parcel.tsx
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Provider as ChakraProvider } from "@/third-party/chakra-ui";
+import { ChakraProvider, SystemContext } from "@chakra-ui/react";
 
 import { AuthControls } from "@/pages/auth";
 
 const queryClient = new QueryClient();
 
-export const parcel = singleSpaReact({
-    React,
-    ReactDOM,
-    rootComponent: () => (
+function App() {
+    const [chakraUiSystem, setChakraUiSystem] = useState<SystemContext | null>(null);
+
+    useEffect(() => {
+        import("@styleguide/react").then((mod) => {
+            setChakraUiSystem(mod.default.chakraUiSystem);
+        });
+    }, []);
+
+    if (!chakraUiSystem) return <div>Loading Chakra...</div>;
+
+    return (
         <QueryClientProvider client={queryClient}>
-            <ChakraProvider>
+            <ChakraProvider value={chakraUiSystem}>
                 <AuthControls />
             </ChakraProvider>
         </QueryClientProvider>
-    ),
+    );
+}
+
+export const parcel = singleSpaReact({
+    React,
+    ReactDOM,
+    rootComponent: App,
     errorBoundary(err, info, props) {
         console.error(err, info, props);
         return <div>Error</div>;
