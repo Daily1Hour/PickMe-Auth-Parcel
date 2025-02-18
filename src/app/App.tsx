27cb95ed3a2ba33d85470672ec3d5ef248dc322e
NarciSource <jwch11@gmail.com>
2025-02-18 13:03:15 +0900

    feat: 개발 모드에 따라 AuthPage와 AuthControls 컴포넌트 전환

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,15 +2,17 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import { pickmeSystem } from "@/shared/thema";
-import AuthPage from "@/pages/auth";
+import AuthPage, { AuthControls } from "@/pages/auth";
 
 const queryClient = new QueryClient();
 
 function App() {
+    const MODE = import.meta.env.MODE;
+
     return (
         <QueryClientProvider client={queryClient}>
             <ChakraProvider value={pickmeSystem}>
-                <AuthPage />
+                {MODE === "development" ? <AuthPage /> : <AuthControls />}
             </ChakraProvider>
         </QueryClientProvider>
     );
--- src/parcel.tsx
+++ src/parcel.tsx
@@ -1,23 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Provider as ChakraProvider } from "@/third-party/chakra-ui";
 
-import { AuthControls } from "@/pages/auth";
-
-const queryClient = new QueryClient();
+import App from "./app/App";
 
 export const parcel = singleSpaReact({
     React,
     ReactDOM,
-    rootComponent: () => (
-        <QueryClientProvider client={queryClient}>
-            <ChakraProvider>
-                <AuthControls />
-            </ChakraProvider>
-        </QueryClientProvider>
-    ),
+    rootComponent: App,
+    errorBoundary(err, info, props) {
+        console.error(err, info, props);
+        return <div>Error</div>;
+    },
 });
 
 export { getTokens, getUser } from "@/entities/auth/service";
