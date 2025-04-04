a88f41cffca309b72a0d5d1ced7fac3f1f5f32d1
NarciSource <jwch11@gmail.com>
2025-04-04 20:15:51 +0900

    refactor: 빌드 통합
    - single-spa 빌드 제거

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-import vitePluginSingleSpa, { SingleSpaPluginOptions } from "vite-plugin-single-spa";
+import vitePluginSingleSpa from "vite-plugin-single-spa";
 
 import userscriptMeta from "./src/userscript/widget.meta";
 
@@ -12,38 +12,23 @@ export default defineConfig(({ mode }) => {
     // 환경변수 증설
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-    const serverPort = Number(process.env.VITE_SERVER_PORT); // 서버포트
-
-    const plugins = [
-        react(), // React 라이브러리 적용
-        tsconfigPaths(), // tsconfig.json의 paths 설정을 적용
-        nodePolyfills(), // node.js 라이브러리 polyfills를 적용
-    ];
-
-    if (mode === "single-spa") {
-        // single-spa 옵션 설정
-        const vitePluginSingleSpaOptions: SingleSpaPluginOptions = {
-            serverPort,
-            spaEntryPoints: "src/main.tsx",
-        };
-
-        // single-spa 빌드 진입점 설정
-        switch (process.env.VITE_MF_TYPE) {
-            case "application":
-                vitePluginSingleSpaOptions.spaEntryPoints = "src/application.tsx";
-                break;
-            case "parcel":
-                vitePluginSingleSpaOptions.spaEntryPoints = "src/parcel.tsx";
-                break;
-        }
-        plugins.push(vitePluginSingleSpa(vitePluginSingleSpaOptions)); // single-spa 라이브러리 적용
-    }
+    const SERVER_PORT = Number(process.env.VITE_SERVER_PORT); // 서버포트
+    const PUBLIC_URL = process.env.VITE_PUBLIC_URL; // 기본경로
 
     // vite 설정
     return {
-        plugins,
+        plugins: [
+            react(), // React 라이브러리 적용
+            tsconfigPaths(), // tsconfig.json의 paths 설정을 적용
+            nodePolyfills(), // node.js 라이브러리 polyfills를 적용
+            vitePluginSingleSpa({
+                // single-spa 라이브러리 적용
+                serverPort: SERVER_PORT,
+                spaEntryPoints: "src/parcel.tsx",
+            }),
+        ],
         build: {
-            target: "esnext",
+            target: "esnext", // 빌드 타겟 설정
             cssMinify: true, // css 코드 압축 여부
             minify: "terser", // 빌드시 코드 압축 여부, 정밀 제어 terser 사용 
             cssCodeSplit: false, // css 코드 분할 여부
@@ -69,12 +54,12 @@ export default defineConfig(({ mode }) => {
         },
         server: {
             // 개발 서버 설정
-            port: serverPort,
+            port: SERVER_PORT,
             cors: true,
         },
         preview: {
-            port: serverPort,
+            port: SERVER_PORT,
         },
-        base: process.env.VITE_PUBLIC_URL,
+        base: PUBLIC_URL,
     };
 });
