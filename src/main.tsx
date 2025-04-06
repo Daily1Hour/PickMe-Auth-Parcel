4c2e04a1b551a9d5d5a18930844aa46617cce047
NarciSource <jwch11@gmail.com>
2025-04-06 18:34:27 +0900

    fix: 전역 스타일 추가 및 AuthPage에서 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import "@/shared/styles";
 import AuthPage from "@/pages/auth";
 import App from "./app/App";
 
