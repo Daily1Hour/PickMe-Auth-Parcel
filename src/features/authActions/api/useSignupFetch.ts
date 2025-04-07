e1f641376f72b2999b9a7cf96344a7857247c235
NarciSource <jwch11@gmail.com>
2025-04-07 19:23:26 +0900

    refactor: useSignupFetch에서 불필요한 상태 제거

--- src/features/authActions/api/useSignupFetch.ts
+++ src/features/authActions/api/useSignupFetch.ts
@@ -5,15 +5,12 @@ import { signup, dto } from "@/entities/auth";
 
 export default function useSignupFetch() {
     const [response, setResponse] = useState<dto.SignupResponse>();
-    const [username, setUsername] = useState<string>();
-    const [email, setEmail] = useState<string>();
 
     const { mutate } = useMutation({
         mutationFn: signup,
         onSuccess: (response: dto.SignupResponse) => {
             if (!response) {
-                console.error("회원가입에 실패했습니다.");
-                return;
+                throw Error("회원가입에 실패했습니다.");
             }
 
             setResponse(response); // 회원가입 성공 시 응답 데이터 저장
@@ -21,10 +18,8 @@ export default function useSignupFetch() {
     });
 
     const submit = (data: dto.SignupRequest) => {
-        setUsername(data.username);
-        setEmail(data.email);
         mutate(data);
     };
 
-    return { submit, response, username, email };
+    return { submit, response };
 }
--- src/features/authActions/ui/SignupForm.tsx
+++ src/features/authActions/ui/SignupForm.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FormProvider } from "react-hook-form";
 import { Stack, Text } from "@chakra-ui/react";
 
+import { SignupResponse } from "@/entities/auth/api/dto";
 import { SignupFieldValues } from "../model";
 import { useSignupForm } from "../hook";
 import { useSignupFetch } from "../api";
@@ -11,12 +12,12 @@ import ConfirmForm from "./ConfirmForm";
 
 export default function SignupForm(): React.ReactElement {
     const methods = useSignupForm(); // 회원가입 폼 상태 및 제출 메서드
-    const { submit: submitSignup, response, username, email } = useSignupFetch(); // 회원가입 API 호출 메서드
+    const { submit, response } = useSignupFetch(); // 회원가입 API 호출 메서드
 
     return (
         <Stack>
             <FormProvider {...methods}>
-                <FormLayout<SignupFieldValues> title="signup" onSubmit={submitSignup}>
+                <FormLayout<SignupFieldValues> title="signup" onSubmit={submit}>
                     <FormField<SignupFieldValues> name="username" />
                     <FormField<SignupFieldValues> name="email" />
                     <FormField<SignupFieldValues> name="password" isSecret />
@@ -24,13 +25,20 @@ export default function SignupForm(): React.ReactElement {
                 </FormLayout>
             </FormProvider>
 
-            {response && (
-                <Stack>
-                    <Text>{email}로 인증 코드가 전송되었습니다.</Text>
+            {response && <Confirm {...response} />}
+        </Stack>
+    );
+}
 
-                    <ConfirmForm username={username!} />
-                </Stack>
-            )}
+function Confirm({
+    codeDeliveryDetails: { Destination: email },
+    user,
+}: Exclude<SignupResponse, undefined>): React.ReactElement {
+    return (
+        <Stack>
+            <Text>{email}로 인증 코드가 전송되었습니다.</Text>
+
+            <ConfirmForm username={user.getUsername()} />
         </Stack>
     );
 }
