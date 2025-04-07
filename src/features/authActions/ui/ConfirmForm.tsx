ce4681e5f016c268263765726820bfff0d4279f6
NarciSource <jwch11@gmail.com>
2025-04-07 19:26:50 +0900

    feat: 인증 코드 확인 후 로그인으로 변경

--- src/features/authActions/ui/ConfirmForm.tsx
+++ src/features/authActions/ui/ConfirmForm.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { FormProvider } from "react-hook-form";
+import { useSetAtom } from "jotai";
 
+import ActionType from "@/shared/ActionType";
 import { ConfirmFieldValues } from "../model";
 import { useConfirmForm } from "../hook";
 import { useConfirmFetch } from "../api";
+import { actionTypeAtom } from "../atom";
 import FormLayout from "./FormLayout";
 import FormField from "./FormField";
 
 export default function ConfirmForm({ username }: { username: string }): React.ReactElement {
+    const setActionType = useSetAtom(actionTypeAtom);
     const methods = useConfirmForm(); // 회원가입 폼 상태 및 제출 메서드
     const { submit } = useConfirmFetch(); // 회원가입 API 호출 메서드
     const onSubmit = async (e: ConfirmFieldValues) => {
         const { message } = await submit({ ...e, username }); // 폼 제출
 
         if (message === "SUCCESS") {
+            setActionType(ActionType.Login); // 액션 타입을 로그인으로 변경
             alert("인증 코드가 확인되었습니다."); // 성공 토스트 메시지
         }
     };
