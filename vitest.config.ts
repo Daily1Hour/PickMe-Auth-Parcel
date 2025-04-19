d3b0d489c35d806c0a148a882cb914d84ddda7a4
NarciSource <jwch11@gmail.com>
2025-04-19 10:20:05 +0900

    fix: 커버리지 저장 경로 변경
    - github pages에서는 레포지토리 베이스 경로 필요
    - vitest 커버리지 reportsDirectory는 경로 마지막 요소만 추출
    - PUBLIC_URL 이용해서 경로 변경

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,31 @@
 import { defineConfig } from "vitest/config";
+import { loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-    plugins: [tsconfigPaths({ loose: true })],
-    test: {
-        reporters: ["verbose", "html"],
-        outputFile: "./test/report.html",
-        coverage: {
-            provider: "istanbul",
-            reporter: ["json-summary", "html"],
-            reportsDirectory: `./test`,
+export default defineConfig(({ mode }) => {
+    // 환경변수 증설
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+    const PUBLIC_URL = process.env.VITE_PUBLIC_URL;
+
+    return {
+        plugins: [tsconfigPaths({ loose: true })],
+        test: {
+            reporters: ["verbose", "html"],
+            outputFile: "./test/report.html",
+            coverage: {
+                provider: "istanbul",
+                reporter: [
+                    "json-summary",
+                    [
+                        "html",
+                        {
+                            subdir: "test/coverage",
+                        },
+                    ],
+                ],
+                reportsDirectory: PUBLIC_URL,
+            },
         },
-    },
+    };
 });
